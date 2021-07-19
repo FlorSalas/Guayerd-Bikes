41b75fb560eff52daa258bdcdc555fc8efd63d35
miss.daee <florchu161@gmail.com>
2021-07-18 22:35:49 -0300

    codigoDescuento terminado

--- js/codigoDescuento.js
+++ js/codigoDescuento.js
@@ -1,9 +1,13 @@
 //codigo descuento
+if(localStorage.getItem('name')){
+    mostrar();
+}
 
 function mostrar() {
     alert(`Estimado/a ${localStorage.getItem('name')}` + " " + "Gracias por elegirnos! Le obsequiamos el código **PROMO** con su compra");
     promo();
 }
+
 
 function promo() {
     let descuento = ["5HOTSALE para obtener un 5% de descuento", "10HOTSALE para obtener un 10% de descuento", "15HOTSALE para obtener un 15% de descuento", "20HOTSALE para obtener un 20% de descuento", "25HOTSALE para obtener un 25% de descuento"];
