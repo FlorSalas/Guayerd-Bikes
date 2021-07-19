ffb52afb7f990a015656bb88a8891efaa30da92b
miss.daee <florchu161@gmail.com>
2021-07-19 14:36:07 -0300

    terminado

--- js/novedades.js
+++ js/novedades.js
@@ -2,35 +2,29 @@ let email = "";
 let seguir = true;
 
 if (!localStorage.getItem('email')) {
-    let novedadesEmail = confirm("¿Desea irecibir un email con novedades?");
+    let novedadesEmail = confirm("¿Desea recibir un email con novedades?");
     localStorage.setItem("novedadesEmail", novedadesEmail);
+        if (novedadesEmail) {
 
-    if (novedadesEmail) {
-
-        while (seguir) {
-            email = prompt("Ingrese su email: ");
-            if (email === "" || !email.includes('@') || !email.includes('.')) {
-                seguir = confirm("Por favor, ingrese una direccion de email válida.");
-            } else {
-                break
+            while (seguir) {
+                email = prompt("Ingrese su email: ");
+                if (email === "" || !email.includes('@') || !email.includes('.')) {
+                    seguir = confirm("Por favor, ingrese una direccion de email válida.");
+                } else {
+                    break
+                }
             }
         }
-    }
-
-    localStorage.setItem('email', email);
-
-    if (novedadesEmail) {
+             localStorage.setItem('email', email);
+        if (novedadesEmail) {
+            let pregunta = confirm("¿Desea ver las ofertas disponibles para usted?");
+            if (pregunta) {
+                window.location.replace("ofertasPersonalizadas.html");
+            }   
+        }
+ } else if (localStorage.getItem('email')) {
         let pregunta = confirm("¿Desea ver las ofertas disponibles para usted?");
         if (pregunta) {
             window.location.replace("ofertasPersonalizadas.html");
         }
-    } else if (localStorage.getItem('email')) {
-        let pregunta = confirm("¿Desea ver las ofertas disponibles para usted?");
-        if (pregunta) {
-            window.location.replace("ofertasPersonalizadas.html");
-        }
-    }
 }
-
-
-i
