122f5f2ab2ed088071d43666709577f14ed9b6b2
miss.daee <florchu161@gmail.com>
2021-07-19 16:23:37 -0300

    todo terminado

--- js/novedades.js
+++ js/novedades.js
@@ -1,30 +1,28 @@
-let email = "";
-let seguir = true;
-
-if (!localStorage.getItem('email')) {
-    let novedadesEmail = confirm("¿Desea recibir un email con novedades?");
-    localStorage.setItem("novedadesEmail", novedadesEmail);
-        if (novedadesEmail) {
-
-            while (seguir) {
-                email = prompt("Ingrese su email: ");
-                if (email === "" || !email.includes('@') || !email.includes('.')) {
-                    seguir = confirm("Por favor, ingrese una direccion de email válida.");
-                } else {
-                    break
-                }
-            }
-        }
-             localStorage.setItem('email', email);
-        if (novedadesEmail) {
-            let pregunta = confirm("¿Desea ver las ofertas disponibles para usted?");
-            if (pregunta) {
-                window.location.replace("ofertasPersonalizadas.html");
-            }   
-        }
- } else if (localStorage.getItem('email')) {
-        let pregunta = confirm("¿Desea ver las ofertas disponibles para usted?");
-        if (pregunta) {
+let seguir=true;
+if(!localStorage.getItem("novedades") && localStorage.getItem("email")){ 
+    let novedades = confirm("Desea recibir novedades por email?");
+    localStorage.setItem("novedades", novedades);
+    if(novedades){
+        let mostrarOfertas = confirm("¿Desea ver las ofertas disponibles para usted?");
+        if(mostrarOfertas){
             window.location.replace("ofertasPersonalizadas.html");
-        }
+        }    
+    }
+}else if(!localStorage.getItem("novedades") && !localStorage.getItem("email") ){
+    let novedades = confirm("Desea recibir novedades por email?");
+    localStorage.setItem("novedades", novedades);
+    if(novedades){
+        while (seguir) {
+            email = prompt("Ingrese su email: ");
+            if (email === "" || !email.includes('@') || !email.includes('.')) {
+            seguir = confirm("Por favor, ingrese una direccion de email válida.");
+            } else {
+                  break
+            }
+            }
+        let mostrarOfertas = confirm("¿Desea ver las ofertas disponibles para usted?");
+        if(mostrarOfertas){
+            window.location.replace("ofertasPersonalizadas.html");
+        }    
+    }
 }
