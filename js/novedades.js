59d6ae3be2acea7ed1abf9ec1d3581062a7f15a4
miss.daee <florchu161@gmail.com>
2021-07-18 22:19:02 -0300

    novedades terminado

--- js/novedades.js
+++ js/novedades.js
@@ -1,11 +1,35 @@
+let email = "";     let seguir=true;
+
 if(!localStorage.getItem('email')){
     let novedadesEmail = confirm("¿Desea irecibir un email con novedades?");
     localStorage.setItem("novedadesEmail", novedadesEmail);
-    if(novedadesEmail){
-        let pregunta= confirm("¿Desea ver las ofertas disponibles para usted?");
-         if(pregunta){
-             window.location.replace("ofertasPersonalizadas.html");
+    
+        if(novedadesEmail){
+        
+            while(seguir){
+                email = prompt("Ingrese su email: ");
+                if (email === "" || !email.includes('@') || !email.includes('.')) {
+                    seguir= confirm("Por favor, ingrese una direccion de email válida.");
+                }else{
+                    break
+                }
+            } 
+        }
+
+        localStorage.setItem('email', email);
+
+        if(novedadesEmail){
+            let pregunta= confirm("¿Desea ver las ofertas disponibles para usted?");
+             if(pregunta){
+                 window.location.replace("ofertasPersonalizadas.html");
+             }
          }
      }
-}
+     
 
+if(localStorage.getItem('email')){
+    let pregunta= confirm("¿Desea ver las ofertas disponibles para usted?");
+             if(pregunta){
+                 window.location.replace("ofertasPersonalizadas.html");
+             }
+}
