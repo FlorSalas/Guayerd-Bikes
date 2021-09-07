33dfded4690f4b71db7e5d3efd87661135a8ca0e
Sergio Elian <sergioelianacosta@gmail.com>
2021-09-06 20:07:27 -0500

    Obtener banner del servidor

---
+++ js/banner.js
@@ -0,0 +1,13 @@
+//Obtener el banner del servidor.
+
+const banner = document.querySelector('#banner');
+
+const renderBanner = (image) => {
+        banner.setAttribute('src', image)
+} 
+
+fetch('http://demo2420474.mockable.io/getHomeBanner')
+.then(response => response.json())
+.then(banner =>{
+    renderBanner(banner.imgUrl)
+});
