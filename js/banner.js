d8d08123efbb5bb99baacf11d5344ea1c73f2302
Sergio Elian <sergioelianacosta@gmail.com>
2021-09-06 22:56:03 -0500

    Tamaño img

--- js/banner.js
+++ js/banner.js
@@ -2,9 +2,8 @@
 
 const banner = document.querySelector('#banner');
 
-const renderBanner = (image) => {
-        banner.setAttribute('src', image)
-} 
+const renderBanner = image => banner.setAttribute('src', image)
+
 
 fetch('http://demo2420474.mockable.io/getHomeBanner')
 .then(response => response.json())
