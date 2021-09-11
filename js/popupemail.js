a4f11c204b1fc8bcdf4dd6994ca7c925c3bd7707
Sergio Elian <sergioelianacosta@gmail.com>
2021-09-11 18:12:43 -0500

    guardar datos de promt a localStorage

--- js/popupemail.js
+++ js/popupemail.js
@@ -5,20 +5,40 @@ var btnAbrirPopup = document.querySelector('#btn-abrir-popup'),
 	popupOff = document.querySelector('.popup-confirm'),
 	popupNo = document.querySelector('#btn-no_abrir-popup');
 
-btnAbrirPopup.addEventListener('click', function(){
+
+const email_form = document.querySelector('#form_email');
+const name_form = document.querySelector('#form_name');
+const erEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//BOTON SI
+btnAbrirPopup.addEventListener('click', function () {
 	overlay.classList.add('active');
 	popup.classList.add('active');
 	popupOff.classList.add('popup-confirm-off');
+	
 });
-
-popupOff.addEventListener('click', function(){
+//BOTON NO
+popupNo.addEventListener('click', function () {
 	popupOff.classList.add('popup-confirm-off');
 });
 
-btnCerrarPopup.addEventListener('click', function(e){
+
+btnCerrarPopup.addEventListener('click', function (e) {
 	e.preventDefault();
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
-}); 
+});
 
+//Obtener valores de los imputs
+name_form.addEventListener('blur', guardar);
+email_form.addEventListener('blur', guardar);
 
+function guardar(e) {
+	if (e.target.type === 'email') {
+		if (erEmail.test(e.target.value)) {
+			localStorage.setItem('valorEmail', e.target.value)
+		}
+	}
+	if(e.target.type === 'text'){
+		localStorage.setItem('valorNombre', e.target.value)
+	}
+}
